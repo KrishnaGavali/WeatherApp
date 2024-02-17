2797ad5575869a4d1275dc27e598a00fe38aa47f
KrishnaGavali <k1810g2005@gmail.com>
2024-02-17 15:54:25 +0530

    SolutionBox Initialized

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import SearchBar from './Components/SearchBar/SearchBar'
+import Solution from './Components/Solution/Solution'
 
 export default function App() {
   return (
     <>
     <SearchBar/>
+    <Solution/>
     
     </>
   )

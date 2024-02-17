8a0f30c9c1820a7989d86ca286e21d9db28416ab
KrishnaGavali <k1810g2005@gmail.com>
2024-02-17 15:24:02 +0530

    SearchBar Completed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import SearchBar from './Components/SearchBar/SearchBar'
 
 export default function App() {
   return (
     <>
-    
+    <SearchBar/>
     </>
   )
 }
---
+++ src/Components/SearchBar/SearchBar.js
@@ -0,0 +1,43 @@
+import React from "react";
+
+export default function SearchBar() {
+  return (
+    <div
+      className="p-2"
+      style={{
+        background: "rgb(255,182,93)",
+        backgroundImage:
+          "linear-gradient(180deg, rgba(255,182,93,1) 0%, rgba(255,88,0,1) 100%)",
+      }}
+    >
+      <div className="text-center">
+        <div className=" mt-3">
+          <h3
+            className=" inline text-white font-semibold mx-10 py-1 border-b-4 rounded"
+            id="navitems1"
+          >
+            Real Time Weather
+          </h3>
+          <h3
+            className=" inline text-white font-semibold mx-10 py-1 hover:border-b-4 transition-all rounded"
+            id="navitems2"
+          >
+            Forecast Weather
+          </h3>
+        </div>
+        <div className="text-center mt-6">
+          <input
+            type="text"
+            name="City Search"
+            id="SearchBar"
+            className=" p-3 mx-1 text-center rounded border-2 border-solid border-black w-[550px]"
+            placeholder="SEARCH FOR CITY .."
+          />
+          <button className="p-3 px-6 border border-black border-solid rounded-md bg-blue-700 text-white mx-[16px] hover:bg-blue-500 transition-all "> 
+            Search
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
